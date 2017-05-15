13952324138ebf8a10a936f89b4def52bbca5383
liweixia <weixiang.li@intel.com>
2017-05-15 16:09:31 +0800

    随机数取值规范性

--- RSA.py
+++ RSA.py
@@ -19,8 +19,8 @@ def get_suijisushu():
 	flag = 0
 	while not flag:
 		n = random.randrange(2**1022,2**1024)
-		#if (n%2 == 0 or n%3 == 0 or n%5 == 0 or n%7 == 0 or n%13 == 0):
-		#	continue
+		if (n%2 == 0 or n%3 == 0 or n%5 == 0 or n%7 == 0 or n%13 == 0):
+			continue
 		flag = aks_sushupanding(n)
 	return n
 

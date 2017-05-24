09633600fac9238ca33276398e7c399a247b2fbf
Icereal0301 <icereal0301@icloud.com>
2017-05-24 12:17:35 +0800

    2048

--- RSA.py
+++ RSA.py
@@ -18,7 +18,7 @@ def aks_sushupanding(n):
 def get_suijisushu():
 	flag = 0
 	while not flag:
-		n = random.randrange(2**1023, 2**1024)
+		n = random.randrange(2**2047, 2**2048)
 		# 剔除部分不可靠因子
 		if (n%2 == 0 or n%3 == 0 or n%5 == 0 or n%7 == 0 or n%13 == 0):
 			continue
